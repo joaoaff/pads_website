86fe8179f2154b1bb7826ba6af45173464549a33
João Alberto <joaoff@gmail.com>
2016-02-28 18:01:57 -0300

    Site funcional. Botoes Inicio, Equipe, Equipamentos, a bandeirinha do Brasil e a cartinha estao funcionando.

--- pads/urls.py
+++ pads/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^$', views.main_pt, name='main_pt'), # e.g. /pads/
     url(r'^equipe/$', views.equipe, name='equipe'), # e.g. /main_pt/equipe/
     url(r'^atividades/$', views.atividades, name='atividades'), # e.g. /main_pt/atividades/
-    url(r'^publicacoes/$', views.equipe, name='publicacoes'), # e.g. /main_pt/publicacoes/
-    url(r'^contato/$', views.equipe, name='contato'), # e.g. /main_pt/contato/
+    url(r'^publicacoes/$', views.publicacoes, name='publicacoes'), # e.g. /main_pt/publicacoes/
+    url(r'^equipamentos/$', views.equipamentos, name='equipamentos'), # e.g. /main_pt/equipamentos/
+    url(r'^contato/$', views.contato, name='contato'), # e.g. /main_pt/contato/
 ]
--- pads/views.py
+++ pads/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Equipamento, Usuario
+
 # Create your views here.
 
 # def index(request):
@@ -8,13 +10,22 @@ def main_pt(request):
     return render(request, 'pads/main_pt.html')
 
 def equipe(request):
-    return HttpResponse("Essa e a pagina da equipe.")
+    professor_lista = Usuario.objects.filter(cargo__cargo__startswith='professor')
+    aluno_lista = Usuario.objects.filter(cargo__cargo__startswith='aluno')
+    context = {'professor_lista': professor_lista,
+               'aluno_lista': aluno_lista,}
+    return render(request, 'pads/equipe_pt.html', context)
 
 def atividades(request):
     return HttpResponse("Essa e a pagina de atividades.")
 
 def publicacoes(request):
     return HttpResponse("Essa e a pagina de publicacoes.")
+
+def equipamentos(request):
+    equipamento_lista = Equipamento.objects.order_by('-categoria')
+    context = {'equipamento_lista': equipamento_lista,}
+    return render(request, 'pads/equipamentos_pt.html', context)
 
 def contato(request):
     return HttpResponse("Essa e a pagina de contato.")
